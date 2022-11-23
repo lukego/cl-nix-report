0d0410cee0e65143c23dd80dbaaab39a0398bae4
Luke Gorrie <luke@nuddy.net>
2022-11-23 21:11:17 +0100

    Pruned more dead code

--- flake.nix
+++ flake.nix
@@ -12,29 +12,6 @@
       inherit (builtins) isAttrs hasAttr attrValues;
       inherit (pkgs.lib) filterAttrs mapAttrs mapAttrs';
       inherit (pkgs.lib.strings) hasPrefix;
-      # from system.package
-      # to   package
-      #unpack = attrs:
-      # from nested: x86_64-linux.foo
-      # to flat:     log-x86_64-linux-foo
-      flatPackages = mapAttrs' (name: deriv: deriv.overrideAttrs (o:
-                                                rec { name = "log-${system}-${deriv.name}";
-                                                   value = deriv.overrideAttrs (o:
-                                                     {
-                                                       inherit name;
-                                                       value = withBuildLog deriv;
-                                                       meta.broken = false;
-                                                     });
-                                                    }));
-      # filter out packages whose dependencies don't build.
-      #
-      # Since recursive nix doesn't seem to work at scale [*] we have no
-      # "try-catch" way to include them in the nix expression evaluation.
-      #
-      # [*] https://github.com/NixOS/nix/issues/7297
-      # CRUDE:
-      eligible = n: d: (isAttrs d) && (hasAttr "systems" d) && hasPrefix "g" n;
-      eligiblePkgs = filterAttrs eligible (nix-cl.packages.${system}.sbcl.pkgs);
       sbclPackages = nix-cl.packages.${system}.sbcl.pkgs;
       logged = sbclPackages.overrideScope'
         (self: super: mapAttrs (name: deriv: withBuildLog deriv) super);
