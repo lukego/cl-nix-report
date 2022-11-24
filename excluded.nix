054d313e0e0057da0de73423848d4010b7d06b94
Luke Gorrie <luke@nuddy.net>
2022-11-24 11:19:27 +0100

    Try adding more lisps and systems

---
+++ excluded.nix
@@ -0,0 +1,30 @@
+# Names of derivations that need to be excluded because they break the build.
+{
+  cl-gtk4-clasp-x86_64-linux                        = true;
+  cl-gtk4_dot_adw-clasp-x86_64-linux                = true;
+  cl-gtk4_dot_webkit2-clasp-x86_64-linux            = true;
+  cl-tar-clasp-x86_64-linux                         = true;
+  cl-tar-file-clasp-x86_64-linux                    = true;
+  clx-truetype-clasp-x86_64-linux                   = true;
+  nyxt-abcl-x86_64-linux                            = true;
+  nyxt-clasp-x86_64-linux                           = true;
+  nyxt-gtk-abcl-x86_64-linux                        = true;
+  nyxt-gtk-clasp-x86_64-linux                       = true;
+  nyxt-ubuntu-package-abcl-x86_64-linux             = true;
+  nyxt-ubuntu-package-clasp-x86_64-linux            = true;
+  qtools-ui-ecl-x86_64-linux                        = true;
+  qtools-ui-spellchecked-text-edit-ecl-x86_64-linux = true;
+  spell-ecl-x86_64-linux                            = true;
+  type-i_dot_test-ecl-x86_64-linux                  = true;
+  type-i_dot_test-clasp-x86_64-linux                = true;
+  defmemo-test-clasp-x86_64-linux                   = true;
+  dot_test-clasp-x86_64-linux                       = true;
+  marshal-tests-clasp-x86_64-linux                  = true;
+  defmemo-test-ccl-x86_64-linux                     = true;
+  facts-abcl-x86_64-linux                           = true;
+  facts-ccl-x86_64-linux                            = true;
+  facts-clasp-x86_64-linux                          = true;
+  facts-ecl-x86_64-linux                            = true;
+  facts-sbcl-aarch64-linux                          = true;
+  facts-sbcl-x86_64-linux                           = true;
+}
--- flake.nix
+++ flake.nix
@@ -6,35 +6,63 @@
 
   outputs = { self, nixpkgs, nix-cl }:
     let
-      withBuildLog = pkgs.callPackage ./withBuildLog.nix pkgs;
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      # [{lisp=string(), system=string()}]
+      variants = [  { lisp = "sbcl";  system = "x86_64-linux";  }
+                    { lisp = "sbcl";  system = "aarch64-linux"; }
+                    { lisp = "clasp"; system = "x86_64-linux";  }
+                    { lisp = "ccl";   system = "x86_64-linux";  }
+                    { lisp = "abcl";  system = "x86_64-linux";  }
+                    { lisp = "ecl";   system = "x86_64-linux";  } ];
+      # derivation() -> derivation()
+      reportSystem = "x86_64-linux";
+      # {string()->derivation()}
+      pkgs = nixpkgs.legacyPackages.${reportSystem};
       inherit (builtins) isAttrs hasAttr attrValues;
-      inherit (pkgs.lib) filterAttrs mapAttrs mapAttrs';
+      inherit (pkgs.lib) filterAttrs mapAttrs mapAttrs' concatMap foldr;
       inherit (pkgs.lib.strings) hasPrefix;
-      sbclPackages = nix-cl.packages.${system}.sbcl.pkgs;
-      logged = sbclPackages.overrideScope'
-        (self: super: mapAttrs (name: deriv: withBuildLog deriv) super);
-      logged' = filterAttrs (name: value: isAttrs value && name != "facts") logged;
+      # {string()->derivation()} -> {string()->derivation()}
+      preprocess = system: lisp-pkgs:
+        # match derivation's system type
+        let withBuildLog = pkgs.callPackage ./withBuildLog.nix nixpkgs.legacyPackages.${system}; in
+        # exclude problematic derivations
+        filterAttrs (_name: isAttrs)
+          # instrument with build-logging
+          (lisp-pkgs.overrideScope'
+            (self: super: mapAttrs (name: deriv:
+              withBuildLog deriv) super));
+      # label packages uniquely by adding system and lisp to name
+      # {lisp=string(),system=string()} -> derivation() -> derivation()
+      labelPackages = {lisp, system}: attrs:
+        mapAttrs' (name: deriv: { name = "${name}-${lisp}-${system}";
+                                  value = deriv; }) attrs;
+      # {lisp=string(), system=string()} -> {string()=>derivation()}
+      labelledPackagesFor = {lisp, system}:
+        labelPackages {inherit lisp system;}
+          (preprocess system nix-cl.packages.${system}.${lisp}.pkgs);
+      excluded = import ./excluded.nix;
+      lispPackages = filterAttrs (name: value: ! hasAttr name excluded)
+        (foldr (a: b: a // b) {} (map labelledPackagesFor variants));
+      #sbclPackages = nix-cl.packages.${system}.sbcl.pkgs;
       report-csv = pkgs.runCommand "report-csv" {} ''
         set -x
         mkdir $out
-        echo "package,system,lisp,status" >> $out/report.csv
+        echo "package,version,system,lisp,lisp_version,status" >> $out/report.csv
         function pkg() {
           status="ok"
           [ -e $1/.LOG/failed ]  && status="failed"
           [ -e $1/.LOG/aborted ] && aborted="aborted"
-          echo $2,$3,sbcl,$status >> $out/report.csv
+          echo $2,$3,$4,$5,$6,$status >> $out/report.csv
         }
         ${pkgs.lib.concatMapStrings (d: ''
-                                          pkg ${d} ${d.pname} ${d.system}
+                                          pkg ${d} ${d.pname} ${d.version} ${d.system} ${d.pkg.pname} ${d.pkg.version}
                                         '')
-          (attrValues logged')}
+          (attrValues lispPackages)}
         mkdir $out/nix-support
         echo "file report $out/report.csv" >> $out/nix-support/hydra-build-products
       '';
     in
       {
-        hydraJobs = { _000-report-csv = report-csv; } // logged';
+        inherit labelledPackagesFor labelPackages lispPackages;
+        hydraJobs = { _000-report-csv = report-csv; } // lispPackages;
       };
 }
